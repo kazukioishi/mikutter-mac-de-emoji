af017dfec594028c367eb136dd4e69b4eba01b8a
Kazuki Oishi <oishikazuki@gmail.com>
2014-05-29 05:18:29 +0900

    こころぴょんぴょんするOS判定を導入しました

--- mikutter-mac-de-emoji.rb
+++ mikutter-mac-de-emoji.rb
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+#こころぴょんぴょん
+$IS_WINDOWS = false
+if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
+  $IS_WINDOWS = true
+end
+
 module Pango
   class << self
     alias parse_markup_org parse_markup
@@ -7,10 +13,18 @@ module Pango
     def parse_markup(text)
       @regex ||= Regexp.new(/([\u{1F300}-\u{1F55B}]+)/) 
 
-      emojied_text = text.gsub(@regex) {
-        "<span font_family=\"Segoe UI Symbol\">#{$1}</span>"
-      }
-
+      if $IS_WINDOWS == true
+        #ご注文はWindowsですか?
+        emojied_text = text.gsub(@regex) {
+          "<span font_family=\"Segoe UI Symbol\">#{$1}</span>"
+        }
+      else
+        #ご注文はMac OS Xですか?
+        emojied_text = text.gsub(@regex) {
+          "<span font_family=\"Apple カラー絵文字\">#{$1}</span>"
+        }
+      end
+      
       parse_markup_org(emojied_text)
     end
   end
